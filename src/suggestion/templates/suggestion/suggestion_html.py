6340d901b5fece3ce55922f85da14302ba0d5c59
Maarten Nieber <hallomaarten@yahoo.com>
2016-02-13 12:04:55 +0100

    Make template more readable

--- src/suggestion/templates/suggestion/suggestion_html.py
+++ src/suggestion/templates/suggestion/suggestion_html.py
@@ -10,26 +10,35 @@ from goal.templates.dominate_tags import *
 from suggestion.templates.dominate_tags import *
 
 
+@div(_class="row small-gap-below")
+def suggestion_image_div():
+    column(4)
+    with column(4):
+        suggestion_image("{{ revision.title }}")
+
+
+@div(_class="row small-gap-below")
+def suggestion_description_div():
+    column(2)
+    with column(8):
+        h5(
+            "Published by {{ suggestion.owner.global_user.name }}, "
+            "{{ suggestion.pub_date|naturaltime }}"
+        )
+        text("{{ revision.description|markdown }}")
+
+
+@div(id="reviews")
+def empty_reviews_div():
+    inline_script(settings.BASE_DIR, 'suggestion/load_reviews.js')
+
+
 def result():
     with django_block("content") as content:
         goal_header()
-
-        with div(_class="row small-gap-below"):
-            column(4)
-            with column(4):
-                suggestion_image("{{ revision.title }}")
-
-        with div(_class="row small-gap-below"):
-            column(2)
-            with column(8):
-                h5(
-                    "Published by {{ suggestion.owner.global_user.name }}, "
-                    "{{ suggestion.pub_date|naturaltime }}"
-                )
-                text("{{ revision.description|markdown }}")
-
-        div(id="reviews")
-        inline_script(settings.BASE_DIR, 'suggestion/load_reviews.js')
+        suggestion_image_div()
+        suggestion_description_div()
+        empty_reviews_div()
 
     return (
         "{% extends 'base.html' %}",
